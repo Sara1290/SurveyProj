2307baec01b658a06f7c188293bfbc23f83e603b
sara reed <sara.reed@previdence.com>
2021-07-09 08:20:04 -0600

    string

--- netlifyFunctions/submit-background.js
+++ netlifyFunctions/submit-background.js
@@ -20,7 +20,7 @@ const { CONNECTION_STRING } = process.env;
 exports.handler = async function (event) {
   survey.onComplete.add(function (sender, options) {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/api/submit');
+    xhr.open("POST", 'postgres://asehehahszkryh:552417fa473d19f6f490b9ef0d2be808db533287543a47a4dd84329f6041cc89@ec2-54-145-102-149.compute-1.amazonaws.com:5432/d5e4h7i0t16jgs');
     xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
     xhr.send(JSON.stringify(sender.data));
     (console.log(sender.data))
