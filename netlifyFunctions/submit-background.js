0209d75211a3647dee34a393bbba06aa4a39115f
sara reed <sara.reed@previdence.com>
2021-07-09 08:13:56 -0600

    netflify function

--- netlifyFunctions/submit-background.js
+++ netlifyFunctions/submit-background.js
@@ -14,3 +14,16 @@ const express = require('express');
 const session = require('express-session');
 // const fetch = require("node-fetch");
 const SurveyCtrl = require('./SurveyCtrl');
+
+const { CONNECTION_STRING } = process.env;
+
+exports.handler = async function (event) {
+  survey.onComplete.add(function (sender, options) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/api/submit');
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhr.send(JSON.stringify(sender.data));
+    (console.log(sender.data))
+});
+
+}
